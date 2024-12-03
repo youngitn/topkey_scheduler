888193d444acd47754aed412f7dd50359063503c
TOPKEY\ytcheng <ytcheng@topkey.com.tw>
2024-12-03 09:32:25 +0800

    test

--- src/main/java/com/topkey/scheduler/App.java
+++ src/main/java/com/topkey/scheduler/App.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class App {
-
+//test
 	public static void main(String[] args) {
 
 		SpringApplication.run(App.class, args);
